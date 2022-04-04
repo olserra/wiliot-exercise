9f385043c1ddb056025eb1c0dbb0823d48e5627c
Otavio Serra <olserra@gmail.com>
2022-04-04 22:45:11 +0100

    Working but missing refactors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import {
+  Line,
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend
+} from 'recharts';
+
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // Creating an empty state
+  const [data, setData] = useState([{}]);
+
+  // Creating a client with WebSocket
+  const ws = new W3CWebSocket('ws://localhost:8999');
+
+  // This code will run every time the component is mounted, and it will re-render every time the state changes, and new data come in from the socket
+  useEffect(() => {
+    ws.onmessage = function (message) {
+      const arr = JSON.parse(message.data);
+      try {
+        // Checking if the connection is ok by checking if the first element is empty
+        if (arr.length > 0) {
+          // Checking if data if bigger then 100
+          if (arr[0].data > 100 || arr[1].data > 100) {
+            return null;
+          } else {
+            const newData = {
+              id_1: arr[0].temperature,
+              id_2: arr[1].temperature,
+              amt: 50,
+              timestamp: message.timestamp
+            }
+            setData((currentData) => {
+              // Save data for 5 minutes then stop saving data
+              if (newData.timestamp >= (currentData[0].timestamp + 30000)) {
+                currentData.shift();
+                currentData.push(newData);
+                return currentData;
+              } else {
+                return [...currentData, newData]
+              }
+            });
+          }
+        }
+      } catch (err) {
+        // Display a toast for connection failed
+        alert('Connection failed')
+      }
+    };
+  }, []);
+
+  // Rendering the chart using the state
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style="container">
+      <div>
+        <div>
+          <p>ID 1</p>
+          <p>{data[0].temperature}</p>
+        </div>
+        <div>
+          <p>ID 2</p>
+          <p>{data[1].temperature}</p>
+        </div>
+      </div>
+      <div>
+        <LineChart width={500} height={300} data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Legend />
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+          <Line type="monotone" dataKey="id_1" stroke="#8884d8" />
+          <Line type="monotone" dataKey="id_2" stroke="#82ca9d" />
+        </LineChart>
+      </div>
+      <h1>DATA</h1>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import WS from "jest-websocket-mock";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("rejects connections that fail the verifyClient option", async () => {
+  new WS("ws://localhost:8999", { verifyClient: () => false });
+  const errorCallback = jest.fn();
+
+  await expect(
+    new Promise((resolve, reject) => {
+      errorCallback.mockImplementation(reject);
+      const client = new WebSocket("ws://localhost:8999");
+      client.onerror = errorCallback;
+      client.onopen = resolve;
+    })
+    // WebSocket onerror event gets called with an event of type error and not an error
+  ).rejects.toEqual(expect.objectContaining({ type: "error" }));
 });
